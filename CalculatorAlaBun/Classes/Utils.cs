9f2e304b00472b89669c397d9bd13ca16cb8f2b4
vancsaervin <57104166+vancsaervin@users.noreply.github.com>
2022-02-06 16:37:26 +0200

    Upload project

---
+++ CalculatorAlaBun/Classes/Utils.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+namespace CalculatorAlaBun.Classes
+{
+    class Utils
+    {
+        static public String ConcatenateStrings(String string1, String string2)
+        {
+            return String.Concat(string1, " ", string2);
+        }
+
+        static public String AddNumber(String result, String number)
+        {
+            if (result.Equals("0"))
+            {
+                result = String.Empty;
+            }
+
+            if (number.Equals(".") && result.Contains("."))
+            {
+                return result;
+            }
+
+            return result + number;
+        }
+
+        static public String AddOperationToInputBox(String input, String operation)
+        {
+            switch (operation)
+            {
+                case "⌫":
+                    input = input.Substring(0, input.Length - 1);
+                    if (input.Length == 0)
+                        return "0";
+                    return input;
+
+                case "CE":
+                    return "0";
+
+                default:
+                    return "0";
+            }
+        }
+
+        static public String AddOperationToHistoryBox(String history, String operation)
+        {
+            switch (operation)
+            {
+                case "C":
+                    return String.Empty;
+
+                default:
+                    return ConcatenateStrings(history, operation);
+            }
+        }
+
+        static public String Calculate(String input, String history)
+        {
+            history = ConcatenateStrings(history, input);
+            System.Object result = new DataTable().Compute(history, null);
+            return result.ToString();
+        }
+
+        static public String CheckEndOfHistoryBox(String history)
+        {
+            const string operators = "+-/*";
+
+            if (operators.Contains(history.Last()))
+                history = history.Remove(history.Length - 1);
+
+            return history;
+        }
+
+        static public String ChangeInputboxSign(String input)
+        {
+            if (input[0].Equals('-'))
+            {
+                input.Trim('-');
+                return input;
+            }
+
+            return String.Concat("-", input);
+        }
+
+        static public String ChangeHistoryBoxSign(String history)
+        {
+            switch (history.Last().ToString())
+            {
+                case "+":
+                    CheckEndOfHistoryBox(history);
+                    history = String.Concat(history, " -");
+                    return history;
+
+                case "-":
+                    CheckEndOfHistoryBox(history);
+                    history = String.Concat(history, " +");
+                    return history;
+
+                default:
+                    return history;
+            }
+        }
+    }
+}
---
+++ CalculatorAlaBun/Views/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using CalculatorAlaBun.Classes;
+
+namespace CalculatorAlaBun.Views
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNumberToInputBox(object sender, RoutedEventArgs e)
+        {
+            String buttonContent = (e.Source as System.Windows.Controls.Button).Content.ToString();
+            String inputText = inputBox.Text;
+            String historyText = historyBox.Text;
+
+            if (historyText.Contains("="))
+            {
+                historyText = String.Empty;
+                inputText = String.Empty;
+            }
+
+            inputText = Utils.AddNumber(inputText, buttonContent);
+            inputBox.Text = inputText;
+            historyBox.Text = historyText;
+        }
+
+        private void AddOperationToInputBox(object sender, RoutedEventArgs e)
+        {
+            String buttonContent = (e.Source as System.Windows.Controls.Button).Content.ToString();
+            String inputText = inputBox.Text;
+            String historyText = historyBox.Text;
+
+            if (inputText.Equals("x / 0 = !Exist"))
+            {
+                historyText = String.Empty;
+                inputText = "0";
+                inputBox.Text = inputText;
+                historyBox.Text = historyText;
+                return;
+            }
+
+            inputText = Utils.AddOperationToInputBox(inputText, buttonContent);
+            inputBox.Text = inputText;
+            historyBox.Text = historyText;
+        }
+
+        private void AddOperationToHistoryBox(object sender, RoutedEventArgs e)
+        {
+            String buttonContent = (e.Source as System.Windows.Controls.Button).Content.ToString();
+            String inputText = inputBox.Text;
+            String historyText = historyBox.Text;
+
+            if (inputText.Equals("x / 0 = !Exist"))
+            {
+                historyText = String.Empty;
+                inputText = "0";
+                inputBox.Text = inputText;
+                historyBox.Text = historyText;
+                return;
+            }
+
+            if (inputText.Equals("0") && !buttonContent.Equals("C"))
+            {
+                return;
+            }
+
+            historyText = Utils.ConcatenateStrings(historyText, inputText);
+            historyText = Utils.AddOperationToHistoryBox(historyText, buttonContent);
+            inputText = "0";
+
+            inputBox.Text = inputText;
+            historyBox.Text = historyText;
+        }
+
+        private void Equals(object sender, RoutedEventArgs e)
+        {
+            String inputText = inputBox.Text;
+            String historyText = historyBox.Text;
+
+            if (String.IsNullOrEmpty(historyText))
+                return;
+
+            String memory = inputText;
+            inputText = Utils.Calculate(inputText, historyText);
+            historyText = String.Concat(historyText, " ", memory, " =");
+
+            if (historyText.Contains("/ 0"))
+                inputText = "x / 0 = !Exist";
+            inputBox.Text = inputText;
+            historyBox.Text = historyText;
+        }
+
+        private void Negative(object sender, RoutedEventArgs e)
+        {
+            String inputText = inputBox.Text;
+            String historyText = historyBox.Text;
+
+            if (inputText.Equals("x / 0 = !Exist"))
+            {
+                inputText = "0";
+                historyText = String.Empty;
+
+                inputBox.Text = inputText;
+                historyBox.Text = historyText;
+                return;
+            }
+
+            String operation = String.Empty;
+            if (!String.IsNullOrEmpty(historyText) && (historyText.Last().ToString().Equals("+") || historyText.Last().ToString().Equals("-")))
+            {
+                historyText = Utils.ChangeHistoryBoxSign(historyText);
+                historyBox.Text = historyText;
+
+                return;
+            }
+
+            inputText = Utils.ChangeInputboxSign(inputText);
+            inputBox.Text = inputText;
+        }
+    }
+}
